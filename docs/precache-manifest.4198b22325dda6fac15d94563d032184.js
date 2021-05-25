9aee5e642b78bb8f7fb8e1810968a68efd2bca4a
Gtz <gantz.mbv@gmail.com>
2021-05-24 19:53:41 -0500

    docs agregado

---
+++ docs/precache-manifest.4198b22325dda6fac15d94563d032184.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd61fa39501623756e983582766449a9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e620632439949ecdc724",
+    "url": "/static/css/main.48d59b0c.chunk.css"
+  },
+  {
+    "revision": "e13293c431772057daf3",
+    "url": "/static/js/2.7fae8f10.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/static/js/2.7fae8f10.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e620632439949ecdc724",
+    "url": "/static/js/main.1000d176.chunk.js"
+  },
+  {
+    "revision": "0a62db85d9147c438712",
+    "url": "/static/js/runtime-main.2bd8def6.js"
+  }
+]);
